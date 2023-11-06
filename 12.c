3f1d886debb6a2035e6709ebd530a912d4d1bd78
dasupoojitha <119743222+dasupoojitha@users.noreply.github.com>
2023-11-06 10:14:54 +0530

    changes

--- 12.c
+++ 12.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-   int x,y=6,z=5;
+   int x,y=5,z=5;
    x=y==z;
    printf("%d",x);
    return 0;
--- 13.c
+++ 13.c
@@ -4,4 +4,5 @@ int main()
     int a=1,b=2,c=5,d=6,e=8,f=9;
     printf("%d",a+b+c+d+e+f);
     return 0;
+    
 }
