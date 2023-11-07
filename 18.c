8828f3d8c240acb489ada25a1133399c8fd19975
dasupoojitha <119743222+dasupoojitha@users.noreply.github.com>
2023-11-07 13:02:40 +0530

    third commit

---
+++ 18.c
@@ -0,0 +1,11 @@
+// C program to illustrate increment
+#include <stdio.h>
+ 
+int main()
+{
+    int a = 5;
+    int b = 5;
+    printf("Pre-Incrementing a = %d\n", ++a);
+    printf("Post-Incrementing b = %d", b++);
+    return 0;
+}
