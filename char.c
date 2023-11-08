607f673709f8af2fc3aab64d158e2604071349d7
dasupoojitha <119743222+dasupoojitha@users.noreply.github.com>
2023-11-08 13:55:00 +0530

    1

--- char.c
+++ char.c
@@ -1,11 +1,12 @@
+// c program using integer and char ans sum of int and char.
 #include <stdio.h>
 int main()
 {
-    int a;
-    char c, sum;
-    a=5;
-    c='p';
-    sum = a + c;
+    int a;//declaring an integer value 
+    char c, sum;// declaring  an char value c
+    a=5;//definining the value a=5
+    c='p';// defining c=p
+    sum = a + c;// declaring  an char value c
     printf(" this is the value of a %d \n", a);
     printf(" this is the value of c %c \n", c);
     printf("the sum a and c is %c \n", a + c);
